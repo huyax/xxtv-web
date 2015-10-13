9f37cf5c155642997c853577dd91baf3302a751f
xx <huyax1990@163.com>
2015-10-13 20:24:49 +0800

    优化结构

--- xxtv-web/src/main/java/com/xxtv/web/controller/IndexController.java
+++ xxtv-web/src/main/java/com/xxtv/web/controller/IndexController.java
@@ -1,6 +1,5 @@
 package com.xxtv.web.controller;
 
-import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -14,91 +13,50 @@ import com.xxtv.base.common.BaseController;
 import com.xxtv.core.plugin.annotation.Control;
 
 @Control(controllerKey = "/")
-public class IndexController extends BaseController
-{
-	private static final Logger	LOGGER	= LoggerFactory.getLogger(IndexController.class);
+public class IndexController extends BaseController {
+	private static final Logger LOGGER = LoggerFactory
+			.getLogger(IndexController.class);
 
-	public void index()
-	{
-		try
-		{
+	public void index() {
+		dota2();
+	}
+
+	public void dota2() {
+		try {
 			String url = "http://www.douyutv.com/api/v1/live/3?aid=android&auth=5b29ce8060866f98416614e8476e14d5&client_sys=android&limit=200&offset=0&time=1444565894";
 			Map result = (Map) JSON.parse(HttpKit.get(url));
 			int error = ((Integer) result.get("error")).intValue();
-			if (error == 0)
-			{
-				List mapList = JSON.parseArray("" + result.get("data"), HashMap.class);
+			if (error == 0) {
+				List mapList = JSON.parseArray("" + result.get("data"),
+						HashMap.class);
 				setAttr("list", mapList);
 			}
 			render("index");
-		}
-		catch (Exception e)
-		{
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
-			if (LOGGER.isErrorEnabled())
-			{
+			if (LOGGER.isErrorEnabled()) {
 				LOGGER.error("index异常：{}", e);
 			}
 			render("index");
 		}
 	}
 
-	public void live()
-	{
-		String nick = getPara("nick");
-		try
-		{
-			nick = new String(nick.getBytes("iso8859-1"), "UTF-8");
-		}
-		catch (UnsupportedEncodingException e)
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		setAttr("uid", getPara("uid"));
-		setAttr("nick", nick);
-		setAttr("id", getPara("id"));
-		render("live");
-	}
-
-	public void lol()
-	{
-		try
-		{
+	public void lol() {
+		try {
 			String url = "http://api.m.huya.com/gamelabel/labellive?limit=40&platform=android&labelid=6&page=1&gameid=1&version=3.2.2";
 			Map result = (Map) JSON.parse(HttpKit.get(url));
 			Map dataMap = (Map) JSON.parse("" + result.get("data"));
-			List mapList = JSON.parseArray("" + dataMap.get("lives"), HashMap.class);
+			List mapList = JSON.parseArray("" + dataMap.get("lives"),
+					HashMap.class);
 			setAttr("list", mapList);
 			render("lol");
-		}
-		catch (Exception e)
-		{
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
-			if (LOGGER.isErrorEnabled())
-			{
+			if (LOGGER.isErrorEnabled()) {
 				LOGGER.error("index异常：{}", e);
 			}
-			render("index");
+			render("lol");
 		}
 	}
-	
-	public void live_lol()
-	{
-		String sNick = getPara("sNick");
-		try
-		{
-			sNick = new String(sNick.getBytes("iso8859-1"), "UTF-8");
-		}
-		catch (UnsupportedEncodingException e)
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		setAttr("lChannelId", getPara("lChannelId"));
-		setAttr("sNick", sNick);
-		setAttr("lSubchannel", getPara("lSubchannel"));
-		setAttr("sAvatarUrl", getPara("sAvatarUrl"));
-		render("live_lol");
-	}
+
 }

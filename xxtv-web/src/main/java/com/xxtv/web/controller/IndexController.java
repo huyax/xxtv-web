461b8762cc0a18afc0c8023ff95afe8db206035c
xx <huyax1990@163.com>
2015-10-14 22:09:02 +0800

    取消iframe方式

--- xxtv-web/src/main/java/com/xxtv/web/controller/IndexController.java
+++ xxtv-web/src/main/java/com/xxtv/web/controller/IndexController.java
@@ -17,77 +17,75 @@ import com.xxtv.tools.EhcacheConstants;
 import com.xxtv.web.model.LiveInterfaceModel;
 
 @Control(controllerKey = "/")
-public class IndexController extends BaseController
-{
-	private static final Logger	LOGGER	= LoggerFactory.getLogger(IndexController.class);
+public class IndexController extends BaseController {
+	private static final Logger LOGGER = LoggerFactory
+			.getLogger(IndexController.class);
 
-	public void index()
-	{
-		render("index");
+	public void index() {
+		douyu();
 	}
 
-	public void yy()
-	{
-		String gameKey = getPara(0);
+	public void yy() {
+		String itemId = getPara(0);
 		String platformKey = "yy";
-		if (gameKey == null)
-		{
-			//默认游戏lol
-			gameKey = "lol";
+		if (itemId == null) {
+			// 默认游戏lol
+			itemId = "1";
 		}
 
-		//直播列表
-		String url = LiveInterfaceModel.dao.getLiveInterface(platformKey, gameKey).getStr("url");
+		setAttr("itemId", itemId);
+		// 直播列表
+		String url = LiveInterfaceModel.dao.getLiveInterface(platformKey,
+				itemId).getStr("url");
 		Map result = (Map) JSON.parse(HttpKit.get(url));
 		String message = (String) result.get("message");
-		if ("success".equals(message))
-		{
-			List mapList = JSON.parseArray("" + result.get("data"), HashMap.class);
+		if ("success".equals(message)) {
+			List mapList = JSON.parseArray("" + result.get("data"),
+					HashMap.class);
 			setAttr("list", mapList);
 		}
 
-		//游戏分类
+		// 游戏分类
 		Object obj = CacheKit.get(EhcacheConstants.GAME_CATALOG, platformKey);
-		if (obj == null)
-		{
+		if (obj == null) {
 			setAttr("cateList", CacheUtil.getYYGameCata(platformKey));
-		}
-		else
-		{
+		} else {
 			setAttr("cateList", (List) obj);
 		}
 
 		render(platformKey);
 	}
 
-	public void douyu()
-	{
+	public void douyu() {
 		String gameKey = getPara(0);
+		String cateId = getPara("cateId");
 		String platformKey = "douyu";
-		if (gameKey == null)
-		{
-			//默认游戏lol
-			gameKey = "dota2";
-		}
 
-		//直播列表
-		String url = LiveInterfaceModel.dao.getLiveInterface(platformKey, gameKey).getStr("url");
+		if (cateId == null && "dota2".equals(gameKey)) {
+			cateId = "3";
+		}
+		setAttr("cateId", cateId == null ? "1" : cateId);
+		setAttr("platformKey", platformKey);
+		if (gameKey == null) {
+			gameKey = "lol";
+		}
+		setAttr("gameKey", gameKey);
+		// 直播列表
+		String url = LiveInterfaceModel.dao.getLiveInterface(platformKey,
+				gameKey).getStr("url");
 		Map result = (Map) JSON.parse(HttpKit.get(url));
 		int error = ((Integer) result.get("error")).intValue();
-		if (error == 0)
-		{
-			List mapList = JSON.parseArray("" + result.get("data"), HashMap.class);
+		if (error == 0) {
+			List mapList = JSON.parseArray("" + result.get("data"),
+					HashMap.class);
 			setAttr("list", mapList);
 		}
 
-		//游戏分类
+		// 游戏分类
 		Object obj = CacheKit.get(EhcacheConstants.GAME_CATALOG, platformKey);
-		if (obj == null)
-		{
+		if (obj == null) {
 			setAttr("cateList", CacheUtil.getDouyuGameCata(platformKey));
-		}
-		else
-		{
+		} else {
 			setAttr("cateList", (List) obj);
 		}
 

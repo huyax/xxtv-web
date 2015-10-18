b8985a7730d08bb80a450b4a7e130de875564f5e
xx <huyax1990@163.com>
2015-10-18 22:35:26 +0800

    增加影片搜索功能

---
+++ xxtv-web/src/main/java/com/xxtv/web/controller/SearchController.java
@@ -0,0 +1,37 @@
+package com.xxtv.web.controller;
+
+import java.io.UnsupportedEncodingException;
+
+import com.jfinal.kit.StrKit;
+import com.jfinal.plugin.activerecord.Page;
+import com.xxtv.base.common.BaseController;
+import com.xxtv.core.plugin.annotation.Control;
+import com.xxtv.web.model.MovieModel;
+
+@Control(controllerKey = "/search")
+public class SearchController extends BaseController {
+
+	public void index() {
+		String q = getPara("q");
+		if (StrKit.isBlank(q)) {
+			redirect("/movie");
+		} else {
+			try {
+				q = new String(q.getBytes("iso8859-1"), "UTF-8");
+			} catch (UnsupportedEncodingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			int pageNum = getPara("page") == null ? 1 : getParaToInt("page");
+			Page<MovieModel> page = MovieModel.dao.search(pageNum, 100, q);
+			setAttr("list", page.getList());
+			setAttr("totalPage", page.getTotalPage());
+			setAttr("currentPage", pageNum);
+			setAttr("total", page.getTotalRow());
+
+			setAttr("q", q);
+			render("search");
+		}
+
+	}
+}
--- xxtv-web/src/main/java/com/xxtv/web/model/MovieModel.java
+++ xxtv-web/src/main/java/com/xxtv/web/model/MovieModel.java
@@ -4,13 +4,23 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.jfinal.plugin.activerecord.Model;
+import com.jfinal.plugin.activerecord.Page;
 import com.xxtv.core.plugin.annotation.Table;
 
 @SuppressWarnings("serial")
 @Table(tableName = "movie")
-public class MovieModel extends Model<MovieModel>
-{
-	private static final Logger			LOGGER	= LoggerFactory.getLogger(MovieModel.class);
+public class MovieModel extends Model<MovieModel> {
+	private static final Logger LOGGER = LoggerFactory
+			.getLogger(MovieModel.class);
 
-	public static final MovieModel	dao		= new MovieModel();
+	public static final MovieModel dao = new MovieModel();
+
+	/**
+	 * 全文索引
+	 * @param q
+	 * @return
+	 */
+	public Page<MovieModel> search(int page,int size,String q) {
+		return dao.paginate(page, size, "SELECT id,name,pub_date ", "FROM `movie`WHERE MATCH (name)AGAINST (?)",q);	
+	}
 }

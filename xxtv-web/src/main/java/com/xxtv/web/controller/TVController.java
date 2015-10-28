0fa99296bac5f88dce28cf9b81604f2c9a8fa817
xx <huyax1990@163.com>
2015-10-28 19:49:32 +0800

    中文乱码

--- xxtv-web/src/main/java/com/xxtv/web/controller/TVController.java
+++ xxtv-web/src/main/java/com/xxtv/web/controller/TVController.java
@@ -1,6 +1,5 @@
 package com.xxtv.web.controller;
 
-
 import java.io.UnsupportedEncodingException;
 import java.util.List;
 
@@ -9,37 +8,41 @@ import com.xxtv.core.plugin.annotation.Control;
 import com.xxtv.web.model.TVModel;
 
 @Control(controllerKey = "/tv")
-public class TVController extends BaseController{
-	
-	public void index()
-	{	
+public class TVController extends BaseController {
+
+	public void index() {
 		String name = getPara("name");
 		String type = getPara("type");
-		if(name == null && type == null){
-			name = "湖南卫视";
-			type = "卫视";
-		}
-		if(name == null && type != null){
-			if(type == "卫视"){
-				name = "湖南卫视";
-			}
-			if(type == "CCTV"){
-				name = "CCTV-1";
-			}
-		}
 		try {
-			name = new String(name.getBytes("iso8859-1"), "UTF-8");
-			type = new String(type.getBytes("iso8859-1"), "UTF-8");
+			if (name != null) {
+				name = new String(name.getBytes("iso8859-1"), "UTF-8");
+			}
+			if (type != null) {
+				type = new String(type.getBytes("iso8859-1"), "UTF-8");
+			}
+			if (name == null && type == null) {
+				name = "湖南卫视";
+				type = "卫视";
+			}
+
+			if (name == null && type != null) {
+				if (type.equals("卫视")) {
+					name = "湖南卫视";
+				}
+				if (type.equals("CCTV")) {
+					name = "CCTV-1";
+				}
+			}
 		} catch (UnsupportedEncodingException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		List<TVModel> lists = TVModel.dao.getTvByType(type);
 		TVModel tvModel = TVModel.dao.getTv(name);
-		setAttr("name",name);
-		setAttr("type",type);
-		setAttr("lists",lists);
-		setAttr("tv",tvModel);
+		setAttr("name", name);
+		setAttr("type", type);
+		setAttr("lists", lists);
+		setAttr("tv", tvModel);
 		render("tv");
 	}
 }
